1e7be3750b84480d943c51085cfc7e3d54c3b738
Alexandre Florentin <lexfloren@gmail.com>
2015-02-28 18:16:41 +0100

    Update Calendrier.java
    Retrait de fonctions de test, documentation étoffée et fonction de validité d'un fichier ics.

--- UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/calendrier/Calendrier.java
+++ UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/calendrier/Calendrier.java
@@ -1,23 +1,7 @@
 package com.mobile.unistra.unistramobile.calendrier;
 
 import com.mobile.unistra.unistramobile.annuaire.Wget;
-
-import net.fortuna.ical4j.data.CalendarBuilder;
-import net.fortuna.ical4j.data.CalendarOutputter;
-import net.fortuna.ical4j.data.CalendarParser;
-import net.fortuna.ical4j.data.CalendarParserImpl;
-import net.fortuna.ical4j.data.ContentHandler;
 import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Component;
-import net.fortuna.ical4j.model.Property;
-
-
-import org.apache.commons.logging.*;
-
-
-import java.util.Iterator;
-import java.io.*;
-import java.net.*;
 
 /**
  * Created by Alexandre on 22-02-15.
@@ -26,11 +10,12 @@ public class Calendrier extends Wget {
     private Calendar calendar;
 
     /**
-     *
-     * @param ressource    La ressource voulue (exemple : "4312" ou "4312,4311")
-     * @param semaines
+     * Constructeur de Calendrier. Il utilise la classe <b>Wget</b> de Nicolas.
+     * <br>À la création, cette classe exécute un thread pour aller chercher des données au format <emph>.ics</emph> sur le site de l'Unistra.
+     * @param ressource     La ressource voulue (exemple : "4312" ou "4312,4311"). 4308 = M1S2 ILC
+     * @param semaines      Je sais pas ce que c'est, ça, probablement la semaine voulue, mais va falloir vérifier...
      */
-    public Calendrier(String ressource, int semaines){
+    public Calendrier(String ressource, String semaines){
         super("https://adewebcons.unistra.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources="+ressource+"&projectId=5&calType=ical&nbWeeks="+semaines);
         this.start();
         try {
@@ -38,56 +23,18 @@ public class Calendrier extends Wget {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //CalendarBuilder builder = new CalendarBuilder();
-
+        //Le tri des informations reçues aura probablement lieu ici
+        this.interrupt();
     }
 
     /**
-     * Cherche un calendrier sur l'ADE et en fait un objet <b>Calendar</b>
-     * @param ressource La ressource voulue (exemple : "4312" ou "4312,4311")
-     * @param nb_semaines
-     * @return Un calendrier, si ça a fonctionné, <b>null</b> sinon.
+     * Permet de vérifier si le fichier téléchargé ressemble bien à un fichier <emph>.ics</emph>.
+     * @return <b>true</b>, si le fichier commence bien par <emph>BEGIN:VCALENDAR</emph> et termine par <emph>END:VCALENDAR</emph>.
      */
-    public static Calendar fetch(String ressource, int nb_semaines){
-        try{
-            CalendarBuilder builder = new CalendarBuilder();
-            URL url = new URL("https://adewebcons.unistra.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?"
-                    + "resources="+ ressource
-                    +"&projectId=5&calType=ical&nbWeeks="+nb_semaines);
-            //URL url = new URL("https://adewebcons.unistra.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4312,4311&projectId=5&calType=ical&nbWeeks=4");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.connect();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-            return builder.build(reader);
-        }catch (Exception e){
-            return null;
-        }
-    }
-
-    /**
-     * Méthode de test qui permet d'itérer sur le calendrier
-     * @param calendar Calendrier à itérer
-     */
-    public static void blabla(Calendar calendar){
-        if(calendar != null){
-            for (Iterator i = calendar.getComponents().iterator(); i.hasNext();) {
-                Component component = (Component) i.next();
-                //System.out.println("Component [" + component.getName() + "]");
-
-                //component.getProperty("DESCRIPTION");
-                System.out.println(component.getProperty("DESCRIPTION").getValue());//+ "\n");
-       /*     	for (Iterator j = component.getProperties().iterator(); j.hasNext();) {
-                	Property property = (Property) j.next();
-                	System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
-            	}*/
-            }
-        }
-    }
-
-    public static String aaa(Calendar calendar){
-        if(calendar != null){
-            return "Calendar = not NULL";
-        }else return "Calendar = NULL";
+    public boolean estValide(){
+        if(this.getHtml().substring(0,15).equalsIgnoreCase("BEGIN:VCALENDAR")
+                && this.getHtml().substring(this.getHtml().length() -13).equalsIgnoreCase("END:VCALENDAR"))
+            return true;
+        else return false;
     }
 }
