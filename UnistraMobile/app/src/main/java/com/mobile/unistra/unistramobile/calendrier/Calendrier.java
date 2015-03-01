02d933da812b0a40d6a0ef1278d331a6c1ec0233
Alexandre Florentin <lexfloren@gmail.com>
2015-03-01 19:55:22 +0100

    Fuseau horaire et virgules
    Interprétation des virgules meilleure et adaptation à l'heure d'hiver/été sur le fuseau horaire de Paris.

--- UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/calendrier/Calendrier.java
+++ UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/calendrier/Calendrier.java
@@ -4,19 +4,19 @@ import com.mobile.unistra.unistramobile.calendrier.Wget;
 import net.fortuna.ical4j.model.Calendar;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by Alexandre on 22-02-15.
  */
 public class Calendrier extends Wget {
-    private Calendar calendar;
-    private Date date;
+    TimeZone fuseauHoraire = TimeZone.getTimeZone("Europe/Paris");
 
     /**
      * Constructeur de Calendrier. Il utilise la classe <b>Wget</b> de Nicolas.
      * <br>À la création, cette classe exécute un thread pour aller chercher des données au format <emph>.ics</emph> sur le site de l'Unistra.
      * @param ressource     La ressource voulue (exemple : "4312" ou "4312,4311"). 4308 = M1S2 ILC
-     * @param semaines      Je sais pas ce que c'est, ça, probablement la semaine voulue, mais va falloir vérifier...
+     * @param semaines      Le nombre de semaines voulues
      */
     public Calendrier(String ressource, String semaines){
         super("https://adewebcons.unistra.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources="+ressource+"&projectId=5&calType=ical&nbWeeks="+semaines);
@@ -51,27 +51,65 @@ public class Calendrier extends Wget {
 
 
     public String nomLieu(String entree){
-        //if(entree.indexOf("TDLOCATION") < entree.indexOf("LOCATION"))
         return entree.substring(entree.indexOf("LOCATION:") + 9, entree.indexOf('\n', entree.indexOf("LOCATION:") + 9));
     }
 
+    /**
+     * L'heure de début.
+     * @param entree
+     * @param date      La date à laquelle l'heure s'applique : permet d'adapter l'heure au fuseau horaire et à l'heure d'hiver/été
+     * @return          Un String contenant l'heure et les minutes, séparés d'un 'h'
+     */
+    public String heureDebut(String entree, Date date){
+        int offset = entree.indexOf("DTSTART:")+17;
+        int heure = Integer.parseInt(entree.substring(offset, offset + 2)) + (fuseauHoraire.getRawOffset() / 3600000);
+
+        //Si on est en heure d'été, on rajoute une heure
+        if(fuseauHoraire.inDaylightTime(date)) heure ++;
+
+        return heure + "h"+ entree.substring(offset +2, offset + 4);
+    }
+
+    /**
+     * L'heure de fin.
+     * @param entree
+     * @param date      La date à laquelle l'heure s'applique : permet d'adapter l'heure au fuseau horaire et à l'heure d'hiver/été
+     * @return          Un String contenant l'heure et les minutes, séparés d'un 'h'
+     */
+    public String heureFin(String entree, Date date){
+        int offset = entree.indexOf("DTEND:")+15;
+        int heure = Integer.parseInt(entree.substring(offset, offset + 2)) + (fuseauHoraire.getRawOffset() / 3600000);
+
+        //Si on est en heure d'été, on rajoute une heure
+        if(fuseauHoraire.inDaylightTime(date)) heure ++;
+
+        return heure + "h"+ entree.substring(offset +2, offset + 4);
+    }
+
     public String jourDebut(String entree){
-        int truc = entree.indexOf("DTSTART:")+14;
-        return entree.substring(truc, truc + 2);
+        int offset = entree.indexOf("DTSTART:")+14;
+        return entree.substring(offset, offset + 2);
     }
 
     public String moisDebut(String entree){
-        int truc = entree.indexOf("DTSTART:")+12;
-        return entree.substring(truc, truc + 2);
+        int offset = entree.indexOf("DTSTART:")+12;
+        return entree.substring(offset, offset + 2);
     }
 
     public String anneeDebut(String entree){
-        int truc = entree.indexOf("DTSTART:")+8;
-        return entree.substring(truc, truc + 4);
+        int offset = entree.indexOf("DTSTART:")+8;
+        return entree.substring(offset, offset + 4);
     }
 
+
     public String afficherEvent(String entree){
-        return nomMatiere(entree) + " : " + jourDebut(entree) + "/" + moisDebut(entree) + "/" + anneeDebut(entree) + ", en salle : "+ nomLieu(entree);
+        int jour = Integer.parseInt(jourDebut(entree));
+        int mois = Integer.parseInt(moisDebut(entree));
+        int annee = Integer.parseInt(anneeDebut(entree));
+        String heureDebut = heureDebut(entree, new Date(annee,mois,jour));
+        String heureFin = heureFin(entree, new Date(annee,mois,jour));
+
+        return nomMatiere(entree) + " : de "+ heureDebut + " à " + heureFin + " le " + jourDebut(entree) + "/" + moisDebut(entree) + "/" + anneeDebut(entree) + ", en salle : "+ nomLieu(entree);
     }
 
     /**
@@ -81,8 +119,11 @@ public class Calendrier extends Wget {
      */
     public ArrayList<String> donnerEvents(){
         String parse = this.getHtml();//.substring(this.getHtml().indexOf("BEGIN:VCALENDAR")+16);
-
         ArrayList<String> resultat = new ArrayList<String>();
+
+        //Les virgules sont représentées par "\,". On va donc remplacer ça par ","
+        parse = parse.replaceAll("\\\\,",",");//Pour échapper un backslash il en faut 4 d'affilée.
+
         while(parse.indexOf("END:VCALENDAR") != 0){
             resultat.add(parse.substring(
                     parse.indexOf("BEGIN:VEVENT")+13,
