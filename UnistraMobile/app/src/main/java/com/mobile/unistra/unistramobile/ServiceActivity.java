9ad2af51dcc0822901d511cb8732c689318db0e9
nbuckenmeier <nicolas.buckenmeier@ctci.fr>
2015-04-23 08:54:10 +0200

    Conversion de ActionBar en activity Le XML est donc inutile

--- UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/ServiceActivity.java
+++ UnistraMobile/app/src/main/java/com/mobile/unistra/unistramobile/ServiceActivity.java
@@ -1,5 +1,6 @@
 package com.mobile.unistra.unistramobile;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +11,7 @@ import android.widget.Button;
 import com.mobile.unistra.unistramobile.service.BackgroundReceiver;
 
 
-public class ServiceActivity extends ActionBarActivity {
+public class ServiceActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +38,5 @@ public class ServiceActivity extends ActionBarActivity {
                 br.setCancelUpdateCal(v.getContext());
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
